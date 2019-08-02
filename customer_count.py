907e5e98f9af05d7b3206d23ab850562169741d1
Aadhil Rushdy <aadhil.rushdy@kohls.com>
2019-08-03 03:05:07 +0530

    changes to customer count

--- customer_count.py
+++ customer_count.py
@@ -12,20 +12,35 @@ def data_read(file_name):
 
 def convert_to_date(x):
     date_str = (x[1]).astype(str)
-    return date_str[-2:]
+    return date_str[0:6]
 
 
-def calculate_customer_count():
-    customer_count = data_read('participants_3a_datathon_44days.csv')
+def calculate_customer_avg():
+
+    customer_count = data_read("participants_3a_datathon_44days.csv")
     print(f"There are {len(customer_count)} records...")
+
     customer_count['hour'] = customer_count.apply(convert_to_date, axis=1)
-    cell_id_mean = customer_count.groupby(["cell_id", "hour"])["cx_cnt"].mean()
-    print(cell_id_mean)
+    print(customer_count)
+
+    customer_count = customer_count.loc[customer_count["hour"] == "201905"]
+    print(customer_count)
+
+    cell_id_mean = customer_count.groupby(["cell_id"])["cx_cnt"].mean()
+    print(f"Cell mean {cell_id_mean}")
+
+    site_info = data_read("datathon.cell.details.csv")
+    cell_site_info = pd.merge(site_info, cell_id_mean, how="left", on=["cell_id"])
+    print(cell_site_info)
+
+    site_mean = cell_site_info.groupby(["site_id"])["cx_cnt"].mean()
+    print(f"Site mean {site_mean}")
 
 
 def main():
-    calculate_customer_count()
+    calculate_customer_avg()
 
 
 if __name__ == "__main__":
     main()
+
